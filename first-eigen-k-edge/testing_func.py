6799f9dddd35e5252f80cefad9eb618462d64791
LetianY <DawnEureka@outlook.com>
2022-10-06 01:38:52 +0800

    Update testing_func.py

--- first-eigen-k-edge/testing_func.py
+++ first-eigen-k-edge/testing_func.py
@@ -2,8 +2,9 @@ import os
 import sys
 import numpy as np
 import networkx as nx
-from pathlib import Path
 from scipy import sparse
+from pathlib import Path
+
 
 module_dur = os.getcwd()
 sys.path.append(module_dur)
@@ -12,52 +13,49 @@ module_path = str(path.parent.absolute())
 sys.path.append(module_path)
 
 
-def generate_graph(n):
-    folder = "../../dataset/testing/"
-    edge_file = folder + "edgelist.txt"
-    G = nx.read_edgelist(edge_file, create_using=nx.DiGraph(), nodetype=int)
-    print(G)
-    # This is redundant for graph construction
-    # for i in range(n):
-    #     G.add_node(i)
-    nx.draw(G)
-    return G
+def generate_graph():
+    folder = "/dataset/testing/"
+    edge_file = module_path + folder + "edgelist.txt"
+    graph = nx.read_edgelist(edge_file, create_using=nx.DiGraph(), nodetype=int)
+    graph = graph.to_undirected()
+    return graph
 
 
-def normalize_laplacian(G):
-    adj = nx.adjacency_matrix(G)
+def normalize_laplacian(graph):
+    # adjacency Matrix
+    adj = nx.adjacency_matrix(graph)
     print(adj.shape)
     ind = range(len(graph.nodes()))
-    print(graph.nodes())
 
-    # degs = D^(-1/2)
-    degs = [1.0 / np.sqrt(graph.out_degree(node) + 1) for node in graph.nodes()]
-    print('step 1:', degs)
-    degs = sparse.csr_matrix(sparse.coo_matrix((degs, (ind, ind)), shape=adj.shape, dtype=np.float))
-    print('step 2:', degs)
+    # degree matrix
+    deg = [graph.degree(node) for node in graph.nodes()]
+    deg = sparse.csr_matrix(sparse.coo_matrix((deg, (ind, ind)), shape=adj.shape, dtype=float))
 
-    # L = D^(-1/2)*(A+I)*D^(-1/2)??
-    L = degs.dot(sparse.eye(adj.shape[0]) + adj)
-    L = L.dot(degs)
+    # normalization: deg_norm = D^(-1/2)
+    deg_norm = [1.0 / np.sqrt(graph.degree(node)) for node in graph.nodes()]
+    deg_norm = sparse.csr_matrix(sparse.coo_matrix((deg_norm, (ind, ind)), shape=adj.shape, dtype=float))
 
-    return L
+    # L = I - D^(-1/2) * A * D^(-1/2)
+    laplacian_norm = sparse.eye(adj.shape[0]) - deg_norm * adj * deg_norm
+
+    return laplacian_norm
 
 
-graph = generate_graph(6)
-L_norm = normalize_laplacian(graph)
+undirected_graph = generate_graph()
+nx.draw(undirected_graph, with_labels=True)
+
+L = nx.laplacian_matrix(undirected_graph)
+L_norm_nx = nx.normalized_laplacian_matrix(undirected_graph)
+L_norm = normalize_laplacian(undirected_graph)
+
+print('#####')
+print(L)
 print('#####')
 print(L_norm)
-
-graph = graph.to_undirected()
-L_norm_nx = nx.normalized_laplacian_matrix(graph)
 print('#####')
 print(L_norm_nx)
 
-L = nx.laplacian_matrix(graph)
 print('#####')
-print(L)
-
+print(nx.laplacian_spectrum(undirected_graph))
 print('#####')
-print(nx.laplacian_spectrum(graph))
-print('#####')
-print(nx.normalized_laplacian_spectrum(graph))
+print(nx.normalized_laplacian_spectrum(undirected_graph))
