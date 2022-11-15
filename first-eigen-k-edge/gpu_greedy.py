f4e48c73707a967d520af9619c7b9ceeb5cdee9e
LetianY <DawnEureka@outlook.com>
2022-11-15 19:08:49 +0800

    Test gpu

--- first-eigen-k-edge/gpu_greedy.py
+++ first-eigen-k-edge/gpu_greedy.py
@@ -1,5 +1,12 @@
 import torch
+from torch_geometric.utils import get_laplacian
 from torch_geometric.utils.convert import from_networkx
+
+def calculate_spectrum_gpu(edge_index):
+    device = torch.device('cuda')
+    lap_sym = get_laplacian(edge_index=edge_index, normalization='sym')
+    lap_sym = lap_sym.to(device)
+
 
 
 def greedy_method_gpu(unused_edges, eigen_val_1st, graph_gcc, output_folder, method, edge_pct):
@@ -14,29 +21,29 @@ def greedy_method_gpu(unused_edges, eigen_val_1st, graph_gcc, output_folder, met
     device = torch.device('cuda')
     temp_graph = from_networkx(graph_gcc)
     temp_graph = temp_graph.to(device)
+    edge_index = temp_graph.edge_index().to(device)
 
-    with Pool(processes=cpu_num) as pool:
-        for i in range(num_add_edges):
-            original_eigen = calculate_spectrum(temp_graph)
+    for i in range(num_add_edges):
+        original_eigen = calculate_spectrum(edge_index)
 
-            # parallel computing, iterate over current unused edges
-            new_spectrum = partial(calculate_new_spectrum, temp_graph)
-            parallel_result = pool.map(new_spectrum, unused_edges)
+        # parallel computing, iterate over current unused edges
+        new_spectrum = partial(calculate_new_spectrum, temp_graph)
+        parallel_result = pool.map(new_spectrum, unused_edges)
 
-            # select edge with max increase for update
-            selected_edge = max(parallel_result)[1]
-            new_eigen = max(parallel_result)[0]
-            edge_sequence.append(selected_edge)
-            eigen_val_sequence.append(new_eigen)
+        # select edge with max increase for update
+        selected_edge = max(parallel_result)[1]
+        new_eigen = max(parallel_result)[0]
+        edge_sequence.append(selected_edge)
+        eigen_val_sequence.append(new_eigen)
 
-            max_increase = new_eigen - original_eigen
-            eigen_increase_sequence.append(max_increase)
-            if i % int(num_add_edges / 100 + 1) == 0:
-                print(f"iteration {i}: max increase = {max_increase}, selected edge = {selected_edge}")
+        max_increase = new_eigen - original_eigen
+        eigen_increase_sequence.append(max_increase)
+        if i % int(num_add_edges / 100 + 1) == 0:
+            print(f"iteration {i}: max increase = {max_increase}, selected edge = {selected_edge}")
 
-            # Delete from unused edge, update graph
-            temp_graph.add_edge(*selected_edge)
-            unused_edges.remove(selected_edge)
+        # Delete from unused edge, update graph
+        temp_graph.add_edge(*selected_edge)
+        unused_edges.remove(selected_edge)
 
     print("saving results...")
     edge_sequence_path = output_folder + f'/{method}/edge_sequence_epct{int(edge_pct*100)}.pkl'
--- first-eigen-k-edge/pytorch_testing.py
+++ first-eigen-k-edge/pytorch_testing.py
@@ -3,7 +3,9 @@ import sys
 from pathlib import Path
 
 import networkx as nx
+import torch
 from torch_geometric.utils.convert import from_networkx
+from torch_geometric.utils import get_laplacian
 
 from utils import graph_preprocessing
 
@@ -17,10 +19,13 @@ folder = "/dataset/"
 edge_file = module_path + folder + "testing/edgelist.txt"
 graph = nx.read_edgelist(edge_file, create_using=nx.DiGraph(), nodetype=int)
 graph_gcc = graph_preprocessing(graph)
-G = nx.convert_node_labels_to_integers(graph_gcc)
-print(graph_gcc.edges())
-print(G.edges())
 pyg_graph = from_networkx(graph_gcc)
 print(pyg_graph)
 print(pyg_graph.is_undirected())
 print(pyg_graph.edge_index)
+
+device = torch.device('cuda')
+lap_sym = get_laplacian(edge_index=pyg_graph.edge_index.to(device), normalization='sym')
+s = torch.sparse_coo_tensor(lap_sym[0], lap_sym[1], (6, 6)).to_dense()
+print(s)
+print(torch.linalg.eigvalsh(s))
